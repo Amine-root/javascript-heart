d97f40949db2dbda1c052707b52c518771d263e0
Amine-root <amine.abouelfath.contacts@gmail.com>
2021-12-02 22:39:49 +0100

    Retrieving data.

--- QuizzzApp/app.js
+++ QuizzzApp/app.js
@@ -1,15 +1,36 @@
 const form = document.querySelector('.form-quizz');
+let tableauResultats = [];
+const reponses = ['c','a','b','a','c'];
+const emojis = ['✔️','✨','👀','😭','👎'];
+const titreResultat = document.querySelector('.resultats h2');
+const noteResultat = document.querySelector('.note');
+const aideResultat = document.querySelector('.aide');
+const toutesLesQuestions = document.querySelectorAll('.question-block');
+let verifTableau = [];
 
-let arrayResult = [];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     for (i = 1; i < 6; i++) {
-        arrayResult.push(document.querySelector(`input[name="q${i}"]:checked`).value);
+        tableauResultats.push(document.querySelector(`input[name="q${i}"]:checked`).value);
     }
-
-    console.log(arrayResult);
-    arrayResult = [];
+    verifFunc(tableauResultats);
+    tableauResultats = [];
  
 });
+
+function verifFunc(tabResultats) {
+    
+    for (let a = 0; a < 5; a++) {
+        if (tableauResultats[a] === reponses[a]) {
+            verifTableau.push(true);
+        } 
+        else 
+        {
+            verifTableau.push(false);
+        }
+    }
+    console.log(verifTableau);
+    verifTableau = [];
+}
